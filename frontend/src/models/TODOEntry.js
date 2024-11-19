483e5dfc5c3a287b710087430ce72fed23a1d1e1
Jaroslav Kopča <kopcajar@fel.cvut.cz>
2024-11-19 15:53:31 +0100

    Model

---
+++ frontend/src/models/TODOEntry.js
@@ -0,0 +1,8 @@
+// models/TODOEntry.js
+export default class TODOEntry {
+    constructor(id, title, isCompleted) {
+        this.id = id;
+        this.title = title;
+        this.isCompleted = isCompleted;
+    }
+}
